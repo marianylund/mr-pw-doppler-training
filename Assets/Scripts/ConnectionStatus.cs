8094c52bdacafca455760b466cd9ac297cdc985e
marianylund <mariaemnylund@gmail.com>
2022-04-16 14:41:33 +0200

    Added BLE status on the probe

---
+++ Assets/Scripts/ConnectionStatus.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ConnectionStatus : MonoBehaviour
+{
+    [SerializeField] private BLEBehaviour _ble;
+    [SerializeField] private Text status;
+
+    private bool prevConnection;
+    
+    private void Update()
+    {
+        if (prevConnection != _ble.isConnected)
+        {
+            prevConnection = _ble.isConnected;
+            BleConnection(_ble.isConnected);
+        }
+    }
+
+    private void BleConnection(bool connected)
+    {
+        if (connected)
+        {
+            status.text = "BLE\nConnected";
+            status.color = Color.Lerp(Color.white, Color.green, 0.3f);
+        }
+        else
+        {
+            status.text = "BLE\nDisconnected";
+            status.color = Color.Lerp(Color.white, Color.red, 0.3f);
+        }
+    }
+
+}
--- Assets/Scripts/SensorFusion.cs
+++ Assets/Scripts/SensorFusion.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using Vuforia;
 
 public class SensorFusion : MonoBehaviour
